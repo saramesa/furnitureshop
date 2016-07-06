cc882f7095ba9aa384499584b2e636a85e3131d7
Sara Mesa <Sara Mesa>
2016-07-06 15:46:28 +0200

    inicio repo

---
+++ js/furnitureJSON.js
@@ -0,0 +1,182 @@
+[
+	{
+		"name": "table1",
+		"image": "images/table1.jpg"
+	},
+	{
+		"name": "table2",
+		"image": "images/table2.jpg"
+	},
+	{
+		"name": "table3",
+		"image": "images/table3.jpg"
+	},
+	{
+		"name": "table4",
+		"image": "images/table4.jpg"
+	},
+	{
+		"name": "table5",
+		"image": "images/table5.jpg"
+	},
+	{
+		"name": "table6",
+		"image": "images/table6.jpg"
+	},
+	{
+		"name": "table7",
+		"image": "images/table7.jpg"
+	},
+	{
+		"name": "table8",
+		"image": "images/table8.jpg"
+	},
+	{
+		"name": "table9",
+		"image": "images/table9.jpg"
+	},
+	{
+		"name": "table10",
+		"image": "images/table10.jpg"
+	},
+	{
+		"name": "table12",
+		"image": "images/table11.jpg"
+	},
+	{
+		"name": "table12",
+		"image": "images/table12.jpg"
+	},
+	{
+		"name": "table13",
+		"image": "images/table13.jpg"
+	},
+	{
+		"name": "table14",
+		"image": "images/table14.jpg"
+	},
+	{
+		"name": "table15",
+		"image": "images/table15.jpg"
+	},
+	{
+		"name": "armario1",
+		"image": "images/armario1.jpg"
+	},
+	{
+		"name": "armario2",
+		"image": "images/armario2.jpg"
+	},
+	{
+		"name": "armario3",
+		"image": "images/armario3.jpg"
+	},
+	{
+		"name": "armario4",
+		"image": "images/armario4.jpg"
+	},
+	{
+		"name": "armario5",
+		"image": "images/armario5.jpg"
+	},
+	{
+		"name": "armario6",
+		"image": "images/armario6.jpg"
+	},
+	{
+		"name": "armario7",
+		"image": "images/armario7.jpg"
+	},
+	{
+		"name": "armario8",
+		"image": "images/armario8.jpg"
+	},
+	{
+		"name": "armario9",
+		"image": "images/armario9.jpg"
+	},
+	{
+		"name": "armario10",
+		"image": "images/armario10.jpg"
+	},
+	{
+		"name": "armario11",
+		"image": "images/armario11.jpg"
+	},
+	{
+		"name": "armario12",
+		"image": "images/armario12.jpg"
+	},
+	{
+		"name": "armario13",
+		"image": "images/armario13.jpg"
+	},
+	{
+		"name": "armario14",
+		"image": "images/armario14.jpg"
+	},
+	{
+		"name": "armario15",
+		"image": "images/armario15.jpg"
+	},
+	{
+		"name": "bookshelf1",
+		"image": "images/bookshelf1.jpg"
+	},
+	{
+		"name": "bookshelf2",
+		"image": "images/bookshelf2.jpg"
+	},
+	{
+		"name": "bookshelf3",
+		"image": "images/bookshelf3.jpg"
+	},
+	{
+		"name": "bookshelf4",
+		"image": "images/bookshelf4.jpg"
+	},
+	{
+		"name": "bookshelf5",
+		"image": "images/bookshelf5.jpg"
+	},
+	{
+		"name": "bookshelf6",
+		"image": "images/bookshelf6.jpg"
+	},
+	{
+		"name": "bookshelf7",
+		"image": "images/bookshelf7.jpg"
+	},
+	{
+		"name": "bookshelf8",
+		"image": "images/bookshelf8.jpg"
+	},
+	{
+		"name": "bookshelf9",
+		"image": "images/bookshelf9.jpg"
+	},
+	{
+		"name": "bookshelf10",
+		"image": "images/bookshelf10.jpg"
+	},
+	{
+		"name": "bookshelf11",
+		"image": "images/bookshelf11.jpg"
+	},
+	{
+		"name": "bookshelf12",
+		"image": "images/bookshelf12.jpg"
+	},
+	{
+		"name": "bookshelf13",
+		"image": "images/bookshelf13.jpg"
+	},
+	{
+		"name": "bookshelf14",
+		"image": "images/bookshelf14.jpg"
+	},
+	{
+		"name": "bookshelf15",
+		"image": "images/bookshelf15.jpg"
+	}
+]
